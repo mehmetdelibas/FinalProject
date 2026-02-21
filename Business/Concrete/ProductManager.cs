440ac7a43753640891d3e77df5cd85fb6d402676
Mehmet Metin Delibas <m.metiin7@gmail.com>
2026-02-22 00:25:17 +0300

    Business Katmanı Tamamlandı.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,22 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal iProductDal)
+        {
+            _productDal = iProductDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //iş kodları, Yetki kontorlü
+            return _productDal.GetAll();
+        }
+    }
+}
