3c055b612e7f59447b2adf083c0d1288937fe405
Mehmet Metin Delibas <m.metiin7@gmail.com>
2026-02-21 23:13:50 +0300

    Entity Katmanı Oluşturuldu

---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Abstract;
+
+namespace Entities.Concrete
+{
+    public class Product : IEntity
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string? ProductName { get; set; }
+        public short UnitINSTock { get; set; }
+        public decimal UnitePrice { get; set; }
+
+    }
+}
